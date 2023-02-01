6bd97f82d5a7634f436781621ad4f65cc2bc7842
Mariia Makarkina <mariiamak25@gmail.com>
2023-02-01 13:38:39 +0200

    git

--- src/components/shared/api.jsx
+++ src/components/shared/api.jsx
@@ -1,15 +1,8 @@
 import axios from 'axios';
 
 export const getImages = async (search, page) => {
-  const { data } = await axios.get('https:pixabay.com/api/', {
-    params: {
-      q: search,
-      page: page,
-      key: '32162387-0406b1794dd4cc3a4c661920a',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      per_page: 12,
-    },
-  });
+  const { data } = await axios.get(
+    `https:pixabay.com/api/?q=${search}&page=${page}&key=32162387-0406b1794dd4cc3a4c661920a&image_type=photo&orientation=horizontal&per_page=12`
+  );
   return data;
 };
